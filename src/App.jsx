de0f29a51ef1caa132362e15e49721c56654e55f
hunain soomro <huniiali1122@gmail.com>
2025-10-25 00:39:47 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>hey hunain how are u</h1>
+      <h1>hey hunain how are soomro sain kese ho bruh</h1>
     </div>
   )
 }
